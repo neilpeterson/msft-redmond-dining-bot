5b0a6472038ec835287050f86252d370a3d07fee
Neil Peterson <neilpeterson@outlook.com>
2016-06-26 00:02:14 -0700

    Adding a second intent

---
+++ redmond-dining-bot/Controllers/menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace redmond_dining_menu
+{
+    public class menu
+    {
+        public string getMenu(string location)
+        {
+            if (location == "building 16")
+            {
+                return "tacos, pizza, fish, cake";
+            }
+            else if (location == "building 25")
+            {
+                return "salad, fruit, pho, sandwich";
+            }
+            else
+            {
+                return "Cannot find a menu for this location.";
+            }
+        }
+    }
+}
