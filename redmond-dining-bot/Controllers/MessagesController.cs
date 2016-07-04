8542b5abdbe9a9fc91941ca432d89a49e4c1608e
Neil Peterson <neilpeterson@outlook.com>
2016-07-04 14:48:13 -0700

    dinign api query and return

--- redmond-dining-bot/Controllers/MessagesController.cs
+++ redmond-dining-bot/Controllers/MessagesController.cs
@@ -9,9 +9,9 @@ using Microsoft.Bot.Connector;
 using Microsoft.Bot.Connector.Utilities;
 using Newtonsoft.Json;
 using DiningLUISNS;
-using redmond_dining_menu;
+using cafemenu;
 using System.Collections.Generic;
-using cafens;
+using cafenamespace;
 using System.Collections;
 
 namespace redmond_dining_bot
@@ -27,8 +27,7 @@ namespace redmond_dining_bot
             if (diLUIS.intents.Count() > 0 && diLUIS.entities.Count() > 0)
             {
                 switch (diLUIS.intents[0].intent)
-                {          
-
+                {   
                     case "find-food": //find-food is an intent from LUIS
                         diningoption = await GetDining(diLUIS.entities[0].entity);
                         break;
@@ -63,8 +62,7 @@ namespace redmond_dining_bot
 
             // De-serialize response into list of objects with type cafe (cafe.cs). 
             List<cafe> list = JsonConvert.DeserializeObject<List<cafe>>(responseBody);
-
-
+            
             // Populate string with cafe’s. 
             foreach (var item in list)
             {
@@ -77,8 +75,29 @@ namespace redmond_dining_bot
 
         private async Task<string> GetMenu(string location)
         {
-            menu menu = new menu();
-            return menu.getMenu(location);
+            // String menu - empty string will be populating from json response.
+            string menu = string.Empty;
+
+            // Unsecure get from dining api.
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync("https://msrefdiningint.azurewebsites.net/api/v1/CafeName/cafe%20" + location + "/items");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            // De-serialize response into list of objects with type cafe (menu.cs).
+            List<Rootobject> list = JsonConvert.DeserializeObject<List<Rootobject>>(responseBody);
+
+            // Populate string with menu item description. 
+            foreach (var item in list)
+            {
+                foreach (var item2 in item.CafeItems)
+                {
+                    menu += item2.Description + "\n\n";
+                }
+            }
+
+            // Return list
+            return menu;
         }
 
         private async Task<DiningLUIS> GetEntityFromLUIS(string Query)
--- redmond-dining-bot/Controllers/menu.cs
+++ redmond-dining-bot/Controllers/menu.cs
@@ -3,24 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace redmond_dining_menu
+namespace cafemenu
 {
-    public class menu
+    public class Rootobject
     {
-        public string getMenu(string location)
-        {
-            if (location == "building 16")
-            {
-                return "tacos, pizza, fish, cake";
-            }
-            else if (location == "building 25")
-            {
-                return "salad, fruit, pho, sandwich";
-            }
-            else
-            {
-                return "Cannot find a menu for this location.";
-            }
-        }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string CafeLogo { get; set; }
+        public string CafeLogoUri { get; set; }
+        public string LocationName { get; set; }
+        public string BuildingFloor { get; set; }
+        public bool IsFoodTruck { get; set; }
+        public string PictureURL { get; set; }
+        public string OperatingHours { get; set; }
+        public Cafeitem[] CafeItems { get; set; }
+        public bool IsMenuItemsAvailable { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class Cafeitem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Prices { get; set; }
+        public string[] WeekDays { get; set; }
+        public int Id { get; set; }
     }
 }
