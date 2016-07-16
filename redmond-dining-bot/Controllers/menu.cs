08c4c9c2bc2bedf5e2947c206c56124d6f89c7ca
Neil Peterson <neilpeterson@outlook.com>
2016-07-16 15:58:43 -0700

    Switched to json in Azure Blob

---
+++ redmond-dining-bot/Controllers/menu.cs
@@ -0,0 +1,20 @@
+﻿namespace msftbot
+{
+    public class menu
+    {
+        public string Name { get; set; }
+        public Cafeitem[] CafeItems { get; set; }
+        public int CafeId { get; set; }
+        public string CafeName { get; set; }
+        public int Id { get; set; }
+    }
+
+    public class Cafeitem
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Prices { get; set; }
+        public string[] WeekDays { get; set; }
+        public int Id { get; set; }
+    }
+}
